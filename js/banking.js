bb5fc3ce5a33c0676a0a8a7dcb470a9c6a2d7e80
kmnazrul <nazrulislam.1292@gmail.com>
2022-04-01 05:34:31 +0600

    nested function - error handling - if else-2

--- js/banking.js
+++ js/banking.js
@@ -17,10 +17,19 @@ function updateTotalField(totalId, amount) {
   totalElement.innerText = previousTotal + amount;
 }
 
-function updateBalance(depositAmount, isAdd) {
+function getCurrentBalance() {
   const balanceTotal = document.getElementById('balance-total');
   const balanceTotalText = balanceTotal.innerText;
   const previousBalanceTotal = parseFloat(balanceTotalText);
+  return previousBalanceTotal;
+}
+
+function updateBalance(depositAmount, isAdd) {
+  const balanceTotal = document.getElementById('balance-total');
+  /* 
+  const balanceTotalText = balanceTotal.innerText;
+  const previousBalanceTotal = parseFloat(balanceTotalText); */
+  const previousBalanceTotal = getCurrentBalance();
   if (isAdd == true) {
     balanceTotal.innerText = previousBalanceTotal + depositAmount;
   } else {
@@ -45,9 +54,14 @@ document
   .getElementById('withdraw-button')
   .addEventListener('click', function () {
     const withdrawAmount = getInputValue('withdraw-input');
+    const currentBalance = getCurrentBalance();
     if (withdrawAmount > 0) {
-      updateTotalField('withdraw-total', withdrawAmount);
-      updateBalance(withdrawAmount, false);
+      if (withdrawAmount < currentBalance) {
+        updateTotalField('withdraw-total', withdrawAmount);
+        updateBalance(withdrawAmount, false);
+      } else {
+        alert('Your balance is insufficient');
+      }
     } else {
       alert('Please input a positive Number');
     }
